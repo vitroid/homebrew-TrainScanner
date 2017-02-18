10c13c72ed284d7912cd7454894af130c3f94a42
vitroid <vitroid@gmail.com>
2017-02-18 14:29:56 +0900

    PyPIに対応．

--- trainscanner.rb
+++ trainscanner.rb
@@ -4,15 +4,15 @@ require "formula"
 
 class Trainscanner < Formula 
   homepage "https://github.com/vitroid/TrainScanner" 
-  url "https://github.com/vitroid/TrainScanner/archive/v0.8.5.tar.gz" 
-  sha256 "d5adbeba9537fcc23949c231d121a9f4a01b93a1" 
+  #url "https://github.com/vitroid/TrainScanner/archive/v0.9.3.tar.gz" 
+  #md5 "792d9c1882a4485689fb7b7c80739322" 
  
   depends_on "pyqt5"
   depends_on "python3"
   depends_on "opencv3" => ["with-ffmpeg", "with-tbb", "with-python3", "HEAD"]
  
   def install 
-    system "make", "all" 
+    system "pip3", "install", "trainscanner" 
   end 
  
   test do 
