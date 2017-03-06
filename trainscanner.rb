d4b2fd55fac95f53dee3528752e2966b2b7538a6
vitroid <vitroid@gmail.com>
2017-03-07 04:20:46 +0900

    。

--- trainscanner.rb
+++ trainscanner.rb
@@ -5,7 +5,7 @@ require "formula"
 class Trainscanner < Formula 
   homepage "https://github.com/vitroid/TrainScanner"
   #It is dummy. Installation will be performed with pip.
-  url "https://github.com/vitroid/homebrew-TrainScanner/archive/v0.1.3.zip" 
+  url "https://github.com/vitroid/homebrew-TrainScanner/archive/v0.1.4.zip" 
   sha256 "eb142499de6cfbb68f556c8d45772605bb4a5162631b17feabfcb89de00d59a2" 
 
   depends_on "pyqt5"
@@ -13,7 +13,6 @@ class Trainscanner < Formula
   depends_on "opencv3" => ["with-ffmpeg", "with-tbb", "with-python3", "HEAD"]
  
   def install 
-    system "brew", "link", "opencv3", "--force"
     system "pip3", "install", "trainscanner"
   end 
  
